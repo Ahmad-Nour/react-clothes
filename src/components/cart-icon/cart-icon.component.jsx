7b26760e90fe2125a2b68db8d505b22b74632b45
ahmad.nour <ahmad.nour@tabtabgo.com>
2023-07-13 00:50:11 +0300

    feat: add cart-icon and cart-dropdown and  cartContext

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,20 @@
+import { CartContext } from '../../contexts/cart.context';
+import { useContext } from 'react';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = () => {
+    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+    const toggleCartOpen = () => setIsCartOpen(!isCartOpen);
+
+    return (
+        <div className='cart-icon-container' onClick={toggleCartOpen}>
+            <ShoppingIcon className='shopping-icon' />
+            <span className='item-count'>0</span>
+        </div>
+    );
+};
+
+export default CartIcon;
--- src/routers/navigation/navigation.jsx
+++ src/routers/navigation/navigation.jsx
@@ -5,9 +5,13 @@ import './navigation.styles.scss';
 import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     const signOutHandler = async () => {
         await signOutUser();
@@ -32,7 +36,11 @@ const Navigation = () => {
                                 SIGN IN
                             </Link>)
                     }
+                    <Link className="nav-link">
+                        <CartIcon />
+                    </Link>
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment >
