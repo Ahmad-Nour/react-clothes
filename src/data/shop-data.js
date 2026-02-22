b885bfb807d87d4569622b65938ef27f6a1c25ca
Ahmad Nour Al-Sabaggh <ahmad.nour.alsabaggh@gmail.com>
2026-02-23 00:53:19 +0300

    edit women data

--- src/data/shop-data.js
+++ src/data/shop-data.js
@@ -152,19 +152,19 @@ const SHOP_DATA = [
             {
                 id: 23,
                 name: 'Blue Tanktop',
-                imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+                imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
                 price: 25,
             },
             {
                 id: 24,
                 name: 'Floral Blouse',
-                imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+                imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
                 price: 20,
             },
             {
                 id: 25,
                 name: 'Floral Dress',
-                imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+                imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
                 price: 80,
             },
             {
@@ -176,19 +176,19 @@ const SHOP_DATA = [
             {
                 id: 27,
                 name: 'Striped Sweater',
-                imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+                imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
                 price: 45,
             },
             {
                 id: 28,
                 name: 'Yellow Track Suit',
-                imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
+                imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
                 price: 135,
             },
             {
                 id: 29,
                 name: 'White Blouse',
-                imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+                imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
                 price: 20,
             },
         ],
